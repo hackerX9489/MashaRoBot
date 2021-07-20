af4e5af7e0609bfcb76958739eb4fd0dc72583f7
hackerX9489 <83834904+hackerX9489@users.noreply.github.com>
2021-07-20 16:44:37 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -87,7 +87,7 @@ buttons = [
     ],
     [
         InlineKeyboardButton(text="ℹ️ ABOUT", callback_data="masha_"),
-        #InlineKeyboardButton(text="📚 COMMANDS", callback_data="help_back"),
+        InlineKeyboardButton(text="📚 COMMANDS", callback_data="help_back"),
     ],
     [
         InlineKeyboardButton(
