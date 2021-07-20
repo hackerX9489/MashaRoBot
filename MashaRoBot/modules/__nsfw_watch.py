46664725a06f1507f279c396e8e7aee36161f45c
hackerX9489 <83834904+hackerX9489@users.noreply.github.com>
2021-07-20 17:14:39 +0530

    Update __nsfw_watch.py

--- MashaRoBot/modules/__nsfw_watch.py
+++ MashaRoBot/modules/__nsfw_watch.py
@@ -111,9 +111,9 @@ async def ws(event):
 
 
 __help__ = """
-Masha can protect your group from NSFW senders
- ❍ /addnsfw*:* Adds The Group to nsfw Watch List
- ❍ /rmnsfw*:* Removes The Group From nsfw Watch List
+Ultron can protect your group from NSFW senders
+ > /addnsfw*:* Adds The Group to nsfw Watch List
+ > /rmnsfw*:* Removes The Group From nsfw Watch List
 """
 
 __mod_name__ = "NSFW"
